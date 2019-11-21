68a09859521f9c7aea73ec201f19de43800dc9d1
GARAM-Y <yeokawa@gmail.com>
2019-11-21 14:26:34 +0900

    메인 프로젝트 > epic-01 : 리베이스 로그 테스트

--- src/GitFlowSampleMain.java
+++ src/GitFlowSampleMain.java
@@ -1,6 +1,5 @@
 public class GitFlowSampleMain {
     static void main(String[] args){
-        System.out.println("sub");
         System.out.println("main");
         System.out.println("리베이스 테스트");
     }
