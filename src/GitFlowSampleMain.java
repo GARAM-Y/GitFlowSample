37405198a89b32a37c4c926f39fc7e19f8ed3fbe
GARAM-Y <yeokawa@gmail.com>
2019-11-21 13:14:08 +0900

    메인 프로젝트 > epic-01 : 포트 테스트 문자 삭제

--- src/GitFlowSampleMain.java
+++ src/GitFlowSampleMain.java
@@ -2,6 +2,5 @@ public class GitFlowSampleMain {
     static void main(String[] args){
         System.out.println("sub");
         System.out.println("main");
-        System.out.println("포크 테스트");
     }
 }
